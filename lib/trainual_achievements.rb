42ede2d2781028c99e008c6e3ce85e6e4957e15c
Oleksandr Zaruba <oleksandr.zaruba@masterofcode.com>
2023-11-04 23:46:51 +0200

    changed credentials

--- lib/trainual_achievements.rb
+++ lib/trainual_achievements.rb
@@ -1,7 +1,7 @@
 class TrainualAchievements
   WEB_LINK_GENERATE_URL = 'https://api.trainual-stg.com/backend/v1/web-links/generate'
-  DEFAULT_USER_ID = 209695
-  TOKEN = "Token token=OroieNYvCVcZh1eoUe2ttlBaqVe2cyOFY1bj5_P5wyyxRFJlI9siOaUmYhDEYXpdly-jGlhWjjAxZ4uZlFLkIQ, email=maksym@trainual.com"
+  DEFAULT_USER_ID = 205964
+  TOKEN = "Token token=EKWqV9gbXGfJJTTgs4q0r_YPWI4iJsztcTqgSRM_pGnWC-ybf_LabwIpjRjlAzcpwK33Ma2tnDfhx24pyiukMQ , email=domingodnls@gmail.com"
   HEADERS = { "accept" => "application/json", "authorization" => TOKEN }
 
   def initialize(params)
