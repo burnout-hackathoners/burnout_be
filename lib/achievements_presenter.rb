8770dc2bef1f531a13c0c010f34c96f2d938be3f
Kravchenko <volodymyr.kravchenko@masterofcode.com>
2023-11-05 14:51:11 +0200

    set limit for external achievements

--- lib/achievements_presenter.rb
+++ lib/achievements_presenter.rb
@@ -15,7 +15,7 @@ class AchievementsPresenter
   private
 
   def collect_all_achievements
-    TrainualAchievements.new(@params).collect + ExternalAchievements.new(@params).collect
+    TrainualAchievements.new(@params).collect + ExternalAchievements.new(@params).collect.first(10)
   end
 
   def calculate_completed_courses(all_achievements)
