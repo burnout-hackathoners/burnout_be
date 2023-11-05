d743fb31b0b75f9970d9fd4dda573c97075e4291
Kravchenko <volodymyr.kravchenko@masterofcode.com>
2023-11-05 09:18:46 +0200

    updated ExternalAchievements class

--- lib/external_achievements.rb
+++ lib/external_achievements.rb
@@ -1,9 +1,196 @@
 class ExternalAchievements
+  FAKE_COURSES_DATA = [
+    {
+      title: "Microsoft Cybersecurity Analyst Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/cc/61dbdf2c1c475d82d3b8bf8eee1bda/MSFT-stacked-logo_FINAL.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "AWS Cloud Technology Consultant Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/a4/7cd68a658840ddbb95c38cdd0bbc8e/aws-logo-icon-PNG-Transparent-Background.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Microsoft Power BI Data Analyst Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/cc/61dbdf2c1c475d82d3b8bf8eee1bda/MSFT-stacked-logo_FINAL.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Meta Front-End Developer Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/73/e03b13a8e44df9b19eb279e5506396/360-x-360.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "IBM IT Project Manager Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/57/2f1c35bd4a4a6d94bde7d622267b9c/Partner-landing-page-Logo-2.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Tableau Business Intelligence Analyst  Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/fe/d443d4baf8419a919a5a0e98e0da28/Untitled-design-1-.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Meta Social Media Marketing Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/73/e03b13a8e44df9b19eb279e5506396/360-x-360.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Google Cybersecurity Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/4a/cb36835ae3421187080898a7ecc11d/Google-G_360x360.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Google Digital Marketing & E-commerce Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/4a/cb36835ae3421187080898a7ecc11d/Google-G_360x360.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Intuit Academy Bookkeeping Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/0e/4bb2b81f8e453eab147bbb94bb53da/Intuit_Square_360x360_WhiteonBlue.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Salesforce Sales Operations Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/72/51d0a967254363b3c2c35be6c09b99/Salesforce-Institution-Square-180px-.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Meta iOS Developer Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/73/e03b13a8e44df9b19eb279e5506396/360-x-360.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Meta Back-End Developer Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/73/e03b13a8e44df9b19eb279e5506396/360-x-360.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Meta Database Engineer Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/73/e03b13a8e44df9b19eb279e5506396/360-x-360.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "IBM Data Science Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/bb/f5ced2bdd4437aa79f00eb1bf7fbf0/IBM-Logo-Blk---Square.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "IBM Cybersecurity Analyst Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/bb/f5ced2bdd4437aa79f00eb1bf7fbf0/IBM-Logo-Blk---Square.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "SAP Technology Consultant  Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/8d/418ea5848b440bb953728e4b76929d/Square-logo-360X360-px.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "HRCI Human Resource Associate Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/e1/e09168d22a4837a71f9b222b3da087/Square-logo-for-partner-landing-page-2-.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "IBM Project Manager  Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/57/2f1c35bd4a4a6d94bde7d622267b9c/Partner-landing-page-Logo-2.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "IBM Front-End Developer Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/bb/f5ced2bdd4437aa79f00eb1bf7fbf0/IBM-Logo-Blk---Square.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Fractal Data Science  Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/d3/88c9b4b1d747a0af3ce22926f5e3e7/1.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Akamai Customer Consulting and Support Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/b3/30b26ed9e048cdac777b90285ab5e7/360x360.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Akamai Network Engineering Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/b3/30b26ed9e048cdac777b90285ab5e7/360x360.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Meta Marketing Analytics Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/73/e03b13a8e44df9b19eb279e5506396/360-x-360.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "CVS Health Retail Customer Service Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/a4/f0324e1ea34a60a08795e815a010c2/Stacked-CVS-Health-logo-1-.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "CVS Health Call Center Customer Service Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/a4/f0324e1ea34a60a08795e815a010c2/Stacked-CVS-Health-logo-1-.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Meta Android Developer Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/73/e03b13a8e44df9b19eb279e5506396/360-x-360.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "IBM DevOps and Software Engineering Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/bb/f5ced2bdd4437aa79f00eb1bf7fbf0/IBM-Logo-Blk---Square.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "IBM Data Engineering Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/bb/f5ced2bdd4437aa79f00eb1bf7fbf0/IBM-Logo-Blk---Square.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "IBM Data Analyst Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/bb/f5ced2bdd4437aa79f00eb1bf7fbf0/IBM-Logo-Blk---Square.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Google Data Analytics Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/4a/cb36835ae3421187080898a7ecc11d/Google-G_360x360.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Google Project Management: Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/4a/cb36835ae3421187080898a7ecc11d/Google-G_360x360.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "IBM Full Stack Software Developer Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/bb/f5ced2bdd4437aa79f00eb1bf7fbf0/IBM-Logo-Blk---Square.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Goodwill® Career Coach and Navigator Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/38/6e7c3221054cf7bc3e1692c67b5dbf/goodwill-logo-nobackground.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "IBM Data Analytics with Excel and R Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/bb/f5ced2bdd4437aa79f00eb1bf7fbf0/IBM-Logo-Blk---Square.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "IBM Back-End Development Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/bb/f5ced2bdd4437aa79f00eb1bf7fbf0/IBM-Logo-Blk---Square.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "IBM IT Support  Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/bb/f5ced2bdd4437aa79f00eb1bf7fbf0/IBM-Logo-Blk---Square.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Google IT Support Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/4a/cb36835ae3421187080898a7ecc11d/Google-G_360x360.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Google UX Design Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/4a/cb36835ae3421187080898a7ecc11d/Google-G_360x360.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Salesforce Sales Development Representative Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/72/51d0a967254363b3c2c35be6c09b99/Salesforce-Institution-Square-180px-.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "IBM Data Warehouse Engineer Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/bb/f5ced2bdd4437aa79f00eb1bf7fbf0/IBM-Logo-Blk---Square.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "Tally Bookkeeper Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/07/4c74de572340098e7f70227b78a59b/360X360_C.png?auto=format%2Ccompress&dpr=1"
+    },
+    {
+      title: "PwC GST Taxation Executive  Professional Certificate",
+      imageUrl: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/44/07370d37c04b58928e27f5e04626d3/Logo-square-institution-landing-360x360-03.png?auto=format%2Ccompress&dpr=1"
+    }
+  ].freeze
+
   def initialize(params)
     @params = params
   end
 
   def collect
-    []
+    uniq_random_ids = generate_uniq_random_ids(FAKE_COURSES_DATA.size)
+    FAKE_COURSES_DATA.map { |course| course.merge(id: uniq_random_ids.shift) }
+  end
+
+  private
+
+  def generate_uniq_random_ids(count)
+    unique_random_ids = []
+    while unique_random_ids.length < count
+      random_id = rand(1..1000)
+      unique_random_ids.push(random_id) if unique_random_ids.exclude?(random_id)
+    end
+    unique_random_ids
   end
 end
